2af466a4960b8879a5eb6c5ac740e44786fa158a
Noven-Austin <158758655+Noven-Austin@users.noreply.github.com>
2024-05-21 02:35:41 +0700

    Update StringManipulator.cs

--- StringManipulator.cs
+++ StringManipulator.cs
@@ -8,5 +8,41 @@ namespace StringManipulationLib
             Array.Reverse(charArray);
             return new string(charArray);
         }
+
+        public static string Analyze(string input)
+        {
+            int characterCount = input.Length;
+            int wordCount = CountWords(input);
+            string reversedString = ReverseString(input);
+
+            return $"Analysis:\n" +
+                   $"- Character Count: {characterCount}\n" +
+                   $"- Word Count: {wordCount}\n" +
+                   $"- Reversed String:\n{reversedString}";
+        }
+
+        private static int CountWords(string input)
+        {
+            int wordCount = 0;
+            bool isWord = false;
+
+            foreach (char c in input)
+            {
+                if (char.IsLetter(c))
+                {
+                    if (!isWord)
+                    {
+                        wordCount++;
+                        isWord = true;
+                    }
+                }
+                else
+                {
+                    isWord = false;
+                }
+            }
+
+            return wordCount;
+        }
     }
 }
